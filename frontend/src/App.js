4da18f2547fdff968ba352c16cc9602e5b7c8408
KishorRShetty <36321276+KishorRShetty@users.noreply.github.com>
2022-07-22 09:15:53 +0000

    pushed through script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,60 @@
-import { useState } from 'react';
-import './App.css';
-import Dice from './components/Dice';
+import { useState } from "react";
+import "./App.css";
+import Dice from "./components/Dice";
+import { nanoid } from "nanoid";
 
 function App() {
-
-  const [dice, setDice] = useState(allNewDice())
+  const [dice, setDice] = useState(allNewDice());
 
   function generateNewDie() {
-    return Math.ceil(Math.random() * 6);
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
   }
 
   function allNewDice() {
     const newDice = [];
-    for (let i = 1; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
       newDice.push(generateNewDie());
     }
     return newDice;
   }
 
+  function holdDice(id) {
+    setDice(oldDice =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? die : generateNewDie()
+  }))
+  }
+
   return (
-    <div className="diceMain">
-      <Dice num="1" />
-      {dice.map(die => (
-        <Dice num={die} />
-      ))}
-    </div>
+    <>
+      <div className="diceMain">
+        {dice.map((die) => (
+          <Dice
+            num={die.value}
+            isHeld={die.isHeld}
+            key={die.id}
+            holdDice={() => holdDice(die.id)}
+          />
+        ))}
+      </div>
+      <button
+        className="btn"
+        onClick={rollDice}
+      >
+        Roll
+      </button>
+    </>
   );
 }
 
--- frontend/src/components/Dice.jsx
+++ frontend/src/components/Dice.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import './Dice.css'
 
 const Dice = (props) => {
+  const styles = {
+    backgroundColor: props.isHeld ? 'tomato' : 'white',
+  }
   return (<>
-    <div className='dice'>{props.num}</div>
+    <div onClick={props.holdDice} style={styles} className='dice'>{props.num}</div>
   </>)
 }
 
