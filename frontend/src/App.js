b8a3336cf1baffc9880433a48d2505152c41affe
KishorRShetty <36321276+KishorRShetty@users.noreply.github.com>
2022-07-22 10:34:03 +0000

    pushed through script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Dice from "./components/Dice";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti"
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
+  const [win, setWin] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const allNumbersSame = dice.every(die => die.value === dice[0].value);
+
+    if (allHeld && allNumbersSame) {
+      setWin(true);
+    }
+  }, [dice])
+
 
   function generateNewDie() {
     return {
@@ -31,13 +43,19 @@ function App() {
   }
 
   function rollDice() {
-    setDice(oldDice => oldDice.map(die => {
-      return die.isHeld ? die : generateNewDie()
-  }))
+
+    if (win) {
+      setWin(false);
+      setDice(allNewDice());
+    } else {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? die : generateNewDie()
+      }))
+    }
   }
 
   return (
-    <>
+    <>{win && <Confetti />}
       <div className="diceMain">
         {dice.map((die) => (
           <Dice
@@ -52,7 +70,7 @@ function App() {
         className="btn"
         onClick={rollDice}
       >
-        Roll
+        {win ? 'New Game' : 'Roll'}
       </button>
     </>
   );
